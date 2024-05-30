f9a444a84cb1271e9ca45755aa38876c93caaef9
hvoiunq <hyojunghan1016@gmail.com>
2024-05-30 13:12:34 +0900

    docs(README) : 하위도메인별로 패키지 생성 및 context 대로 모듈 이동

--- src/main/java/kitchenpos/menu/domain/Menu.java
+++ src/main/java/kitchenpos/menu/domain/Menu.java
@@ -1,4 +1,4 @@
-package kitchenpos.domain;
+package kitchenpos.menu.domain;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -10,6 +10,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
+import kitchenpos.domain.MenuProduct;
 
 import java.math.BigDecimal;
 import java.util.List;
